9b3b0a9b3a6dd99330918782574ae19d30e125cd
JalalLiinuX <smjjalalzadeh93@gmail.com>
2023-11-03 12:42:43 +0330

    .

--- Api/Migrations/20231103084709_InitialCreate.cs
+++ Api/Migrations/20231103084709_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Data.Migrations
+namespace Api.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
